c634f3a8d41f19132f443a11d0f99d94e2c671d6
Jayshiv Patel <jdp221@ic.ac.uk>
2024-11-18 20:58:38 +0000

    Add gitmodule RecurrenceRelationshipArrays

---
+++ src/recurrance-arrays.jl
@@ -0,0 +1,16 @@
+using RecurrenceRelationshipArrays, FillArrays, InfiniteArrays
+
+# Choose a point close to the support
+z = 1.0001;
+
+# Recurrence coefficients for Legendre
+rec_P = (1:∞), (1:2:∞), -1 * (1:∞);
+
+# Exaact formula for Stieltjes transform of sqrt(1 - x²)
+stieltjes = inv(z + sign(z)sqrt(z^2 - 1));
+
+@time r = RecurrenceArray(z, rec_P, [stieltjes, stieltjes^2]);
+
+
+
+
