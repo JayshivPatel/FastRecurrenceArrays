e0e4ef9294a23c6ef4d2bde002648c73053257a4
Jayshiv Patel <jayshivpatel@hotmail.co.uk>
2025-04-15 15:37:21 +0100

    Add integrals + bugfix with data. Add integral correctness tests.

---
+++ test/integraltests.jl
@@ -0,0 +1,20 @@
+# Basic unit tests
+
+using ClassicalOrthogonalPolynomials, 
+    FastRecurrenceArrays,
+    RecurrenceRelationships,
+    RecurrenceRelationshipArrays,
+    SingularIntegrals,
+    Test;
+
+x = [0.1+0im, 1.0001, 10.0];
+N = 15;
+P = Legendre(); x = axes(P, 1); f = expand(P, exp); ff = collect(f.args[2][1:N-2]);
+
+@testset "Stieltjes" begin
+    @test inv.(10 .- x') * f ≈ FastStieltjes(N, [10.0], ff).f[1]
+end
+
+@testset "LogKernel" begin
+    @test log.(abs.(10 .- x')) * f ≈ FastLogKernel(N, [10.0], ff).f[1]
+end
