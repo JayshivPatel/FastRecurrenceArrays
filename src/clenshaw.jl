c5d059742a93976fb804e95b5c0e24a559fa7e80
Jayshiv Patel <jayshivpatel@hotmail.co.uk>
2025-05-20 15:45:08 +0100

    Fix columnwise clenshaw

--- src/clenshaw.jl
+++ src/clenshaw.jl
@@ -1,4 +1,4 @@
-import RecurrenceRelationships: check_clenshaw_recurrences, clenshaw_next, _clenshaw_first
+import RecurrenceRelationships: check_clenshaw_recurrences, clenshaw_next, _clenshaw_first, clenshaw
 import CUDA: CuArray
 
 export FixedClenshaw, ThreadedClenshaw, GPUClenshaw
@@ -51,9 +51,8 @@ function Clenshaw(c::AbstractVector, (A, B, C), x::AbstractVector,
     return f
 end
 
-
 function _GPUClenshaw(c::AbstractVector, (A, B, C), x::AbstractVector,
-    p0::Union{AbstractVector, Nothing}, p1::Union{AbstractVector, Nothing})
+    p0::Nothing, p1::Nothing)
 
     # enforce Float32/ComplexF32
     c = checkandconvert(c)
@@ -61,14 +60,6 @@ function _GPUClenshaw(c::AbstractVector, (A, B, C), x::AbstractVector,
     B = checkandconvert(B)
     C = checkandconvert(C)
     x = checkandconvert(x)
-
-    if p0 !== nothing && p1 !== nothing
-        p0 = checkandconvert(p0)
-        p1 = checkandconvert(p1)
-
-        gpu_p0 = CuArray(p0)
-        gpu_p1 = CuArray(p1)
-    end
 
     num_points = length(x)
     num_coeffs = length(c)
@@ -97,12 +88,51 @@ function _GPUClenshaw(c::AbstractVector, (A, B, C), x::AbstractVector,
 
     @. gpu_bn0 = c[1] + (A[1] * gpu_x + B[1]) * gpu_bn1 - C[2] * gpu_bn2
 
-    if p0 === nothing && p1 === nothing
-        return gpu_bn0
-    else
-        # fₓ ≈ b₀(x)p₀(x) + b₁(x)(p₁(x) - α₀(x)p₀(x))
-        @. gpu_f = (gpu_bn0 * gpu_p0) + gpu_bn1 * (gpu_p1 - (A[1] * gpu_x + B[1]) * gpu_p0)
+    return gpu_bn0
+end
+
+function _GPUClenshaw(c::AbstractVector, (A, B, C), x::AbstractVector,
+    p0::AbstractVector, p1::AbstractVector)
+
+    # enforce Float32/ComplexF32
+    c = checkandconvert(c)
+    A = checkandconvert(A)
+    B = checkandconvert(B)
+    C = checkandconvert(C)
+    x = checkandconvert(x)
+    p0 = checkandconvert(p0)
+    p1 = checkandconvert(p1)
+
+    num_points = length(x)
+    num_coeffs = length(c)
+    T = eltype(x)
+
+    @boundscheck check_clenshaw_recurrences(num_coeffs, A, B, C)
+
+    num_coeffs == 0 && return CUDA.zeros(T, num_points)
+
+    gpu_x = CuArray(x)
+    gpu_bn2 = CUDA.zeros(T, num_points)
+    gpu_bn1 = CUDA.fill(convert(T, c[num_coeffs]), num_points)
+    gpu_next = CuArray{T}(undef, num_points)
+    gpu_f = CuArray{T}(undef, num_points)
+    gpu_p0 = CuArray(p0)
+    gpu_p1 = CuArray(p1)
+
+    num_coeffs == 1 && return gpu_bn1
+
+    @inbounds for n = num_coeffs-1:-1:2
+        # bₙ(x) = fₙ + (Aₙx + Bₙ)bₙ₊₁(x) - Cₙ₊₁bₙ₊₂(x) 
+        @. gpu_next = c[n] + (A[n] * gpu_x + B[n]) * gpu_bn1 - C[n+1] * gpu_bn2
+
+        @. gpu_bn2 = gpu_bn1
+        @. gpu_bn1 = gpu_next
     end
+
+    @. gpu_next = c[1] + (A[1] * gpu_x + B[1]) * gpu_bn1 - C[2] * gpu_bn2
+
+    # fₓ ≈ b₀(x)p₀(x) + b₁(x)(p₁(x) - α₀(x)p₀(x))
+    @. gpu_f = (gpu_next * gpu_p0) + gpu_bn1 * (gpu_p1 - (A[1] * gpu_x + B[1]) * gpu_p0)
 
     return gpu_f
 end
@@ -110,55 +140,11 @@ end
 function clenshaw!(f::AbstractVector, c::AbstractVector, (A, B, C), x::AbstractVector,
     p0::Union{AbstractVector, Nothing}, p1::Union{AbstractVector, Nothing})
 
-    num_points = length(x)
-    num_coeffs = length(c)
-
-    T = eltype(x)
-
-    @boundscheck check_clenshaw_recurrences(num_coeffs, A, B, C)
-
-    num_coeffs == 0 && return zero(T)
-
-    b₀ = Base.zeros(T, num_points)
-
-    if p0 === nothing && p1 === nothing
-        @inbounds for j in axes(x, 1)
-            bn2 = zero(T)
-            bn1 = convert(T, c[num_coeffs])
-
-            num_coeffs == 1 && (b₀[j] = bn1; continue)
-
-            for n = num_coeffs-1:-1:2
-                bn1,bn2 = clenshaw_next(n, A, B, C, x[j], c, bn1, bn2), bn1
-            end
-
-            b₀[j] = _clenshaw_first(A, B, C, x[j], c, bn1, bn2)
-        end
-        
-        @. f = b₀
-    else
-        b₁ = Base.zeros(T, num_points)
-        @inbounds for j in axes(x, 1)
-            bn2 = zero(T)
-            bn1 = convert(T, c[num_coeffs])
-
-            num_coeffs == 1 && (b₀[j] = bn1; continue)
-
-            for n = num_coeffs-1:-1:2
-                bn1,bn2 = clenshaw_next(n, A, B, C, x[j], c, bn1, bn2), bn1
-            end
-
-            b₀[j] = _clenshaw_first(A, B, C, x[j], c, bn1, bn2)
-            b₁[j] = bn1
-        end
-
-        # fₓ ≈ b₀(x)p₀(x) + b₁(x)(p₁(x) - α₀(x)p₀(x))
-        @. f = (b₀ * p0) + b₁ * (p1 - (A[1] * x + B[1]) * p0)
-    end
+    f .= clenshaw_data.(Ref(c), Ref((A, B, C)), x, p0, p1)
 end
 
 function rowthreadedclenshaw!(f::AbstractVector, c::AbstractVector, (A, B, C), x::AbstractVector,
-    p0::Union{AbstractVector, Nothing}, p1::Union{AbstractVector, Nothing})
+    p0::Nothing, p1::Nothing)
 
     num_points = length(x)
     num_coeffs = length(c)
@@ -171,7 +157,6 @@ function rowthreadedclenshaw!(f::AbstractVector, c::AbstractVector, (A, B, C), x
 
     bn2 = Base.zeros(T, num_points)
     bn1 = Base.fill(convert(T, c[num_coeffs]), num_points)
-    bn0 = Array{T}(undef, num_points)
     next = Array{T}(undef, num_points)
 
     num_coeffs == 1 && return bn1
@@ -186,18 +171,11 @@ function rowthreadedclenshaw!(f::AbstractVector, c::AbstractVector, (A, B, C), x
         @. bn1 = next
     end
 
-    @. bn0 = c[1] + (A[1] * x + B[1]) * bn1 - C[2] * bn2
-    
-    if p0 === nothing && p1 === nothing
-        @. f = bn0
-    else
-        # fₓ ≈ b₀(x)p₀(x) + b₁(x)(p₁(x) - α₀(x)p₀(x))
-        @. f = (bn0 * p0) + bn1 * (p1 - (A[1] * x + B[1]) * p0)
-    end
+    @. f = c[1] + (A[1] * x + B[1]) * bn1 - C[2] * bn2
 end
 
-function columnthreadedclenshaw!(f::AbstractVector, c::AbstractVector, (A, B, C), x::AbstractVector,
-    p0::Union{AbstractVector, Nothing}, p1::Union{AbstractVector, Nothing})
+function rowthreadedclenshaw!(f::AbstractVector, c::AbstractVector, (A, B, C), x::AbstractVector,
+    p0::AbstractVector, p1::AbstractVector)
 
     num_points = length(x)
     num_coeffs = length(c)
@@ -208,40 +186,70 @@ function columnthreadedclenshaw!(f::AbstractVector, c::AbstractVector, (A, B, C)
 
     num_coeffs == 0 && return zero(T)
 
-    b₀ = Base.zeros(T, num_points)
+    bn2 = Base.zeros(T, num_points)
+    bn1 = Base.fill(convert(T, c[num_coeffs]), num_points)
+    next = Array{T}(undef, num_points)
 
-    if p0 === nothing && p1 === nothing
-        @inbounds Threads.@threads for j in axes(x, 1)
-            bn2 = zero(T)
-            bn1 = convert(T, c[num_coeffs])
+    num_coeffs == 1 && return bn1
 
-            num_coeffs == 1 && (b₀[j] = bn1; continue)
-
-            for n = num_coeffs-1:-1:2
-                bn1,bn2 = clenshaw_next(n, A, B, C, x[j], c, bn1, bn2), bn1
-            end
-
-            b₀[j] = _clenshaw_first(A, B, C, x[j], c, bn1, bn2)
+    @inbounds for n = num_coeffs-1:-1:2
+        # bₙ(x) = fₙ + (Aₙx + Bₙ)bₙ₊₁(x) - Cₙ₊₁bₙ₊₂(x) 
+        Threads.@threads for j in axes(x, 1)
+            next[j] = c[n] + (A[n] * x[j] + B[n]) * bn1[j] - C[n+1] * bn2[j]
         end
         
-        @. f = b₀
-    else
-        b₁ = Base.zeros(T, num_points)
-        @inbounds Threads.@threads for j in axes(x, 1)
-            bn2 = zero(T)
-            bn1 = convert(T, c[num_coeffs])
+        @. bn2 = bn1
+        @. bn1 = next
+    end
 
-            num_coeffs == 1 && (b₀[j] = bn1; continue)
+    @. next = c[1] + (A[1] * x + B[1]) * bn1 - C[2] * bn2
+    
+    # fₓ ≈ b₀(x)p₀(x) + b₁(x)(p₁(x) - α₀(x)p₀(x))
+    @. f = (next * p0) + bn1 * (p1 - (A[1] * x + B[1]) * p0)
+end
 
-            for n = num_coeffs-1:-1:2
-                bn1,bn2 = clenshaw_next(n, A, B, C, x[j], c, bn1, bn2), bn1
-            end
+function columnthreadedclenshaw!(f::AbstractVector, c::AbstractVector, (A, B, C), x::AbstractVector,
+    p0::Nothing, p1::Nothing)
 
-            b₀[j] = _clenshaw_first(A, B, C, x[j], c, bn1, bn2)
-            b₁[j] = bn1
+    @inbounds Threads.@threads for j in axes(x, 1)
+        f[j] = clenshaw_data(c, (A, B, C), x[j], p0, p1)
+    end
+end
+
+function columnthreadedclenshaw!(f::AbstractVector, c::AbstractVector, (A, B, C), x::AbstractVector,
+    p0::AbstractVector, p1::AbstractVector)
+
+    @inbounds Threads.@threads for j in axes(x, 1)
+        f[j] = clenshaw_data(c, (A, B, C), x[j], p0[j], p1[j])
+    end
+end
+
+function clenshaw_data(c::AbstractVector, (A, B, C), x::Number, p0::Number, p1::Number)
+    num_coeffs = length(c)
+
+    T = eltype(x)
+
+    @boundscheck check_clenshaw_recurrences(num_coeffs, A, B, C)
+
+    num_coeffs == 0 && return zero(T)
+
+    @inbounds begin
+        bn2 = zero(T)
+        bn1 = convert(T, c[num_coeffs])
+
+        num_coeffs == 1 && return bn1
+
+        for n = num_coeffs-1:-1:2
+            bn1,bn2 = clenshaw_next(n, A, B, C, x, c, bn1, bn2), bn1
         end
 
-        # fₓ ≈ b₀(x)p₀(x) + b₁(x)(p₁(x) - α₀(x)p₀(x))
-        @. f = (b₀ * p0) + b₁ * (p1 - (A[1] * x + B[1]) * p0)
+        bn1,bn2 = _clenshaw_first(A, B, C, x, c, bn1, bn2), bn1
     end
+
+    # fₓ ≈ b₀(x)p₀(x) + b₁(x)(p₁(x) - α₀(x)p₀(x))
+    return (bn1 * p0) + bn2 * (p1 - (A[1] * x + B[1]) * p0)
+end
+
+function clenshaw_data(c::AbstractVector, (A, B, C), x::Number, p0::Nothing, p1::Nothing)
+    return clenshaw(c, A, B, C, x)
 end
